0171fe6fd84cad2b0c1605a4bf1b5bdc51a5456b
linszay <lindseyjlancaster@gmail.com>
2022-10-11 15:13:03 -0500

    justify text

---
+++ c_programs/rules.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int main ()
+{
+    printf("Braces come in pairs!\n");
+    printf("All statements end with a semicolon!\n");
+    printf("Spaces are optional!\n");
+    printf("Must have a main function!\n");
+    printf("C is done mostly in lowercase.\nIt's a case sensitive language.");
+
+    return (0);
+}
